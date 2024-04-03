7aa6182480d0b821cf28d060cae0d874db25139a
Tali Auster <me@tali.network>
2024-04-03 11:45:42 -0600

    tree: retry using alire v2
    Previous commits say that I updated Alire to 2.0.0, but apparently I
    wasn't being careful, and I lied. Builds work now :)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default =
