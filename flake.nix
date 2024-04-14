edc7f4041b7fead9136e8ec6cbe3350761bf271b
Tali Auster <me@tali.network>
2024-04-14 14:06:53 -0600

    repo: use gnatprove 13

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default =
       let pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in pkgs.mkShell {
         buildInputs = with pkgs; [
-          alire gprbuild gnat libxcrypt
+          alire gnat13Packages.gnatprove gnat13 libxcrypt
           spark2014 z3 cvc4
         ];
       };
